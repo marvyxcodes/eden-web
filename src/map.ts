f6f1be82217d137ccfd0f45157c6b52e1c24ada8
marv <trujillomarvin@hotmail.com>
2025-01-06 15:29:34 -0600

    changes to file structure/styling

--- src/map.ts
+++ src/map.ts
@@ -1,4 +1,17 @@
 import L from "leaflet";
+// import { checkForLocation, setupMap } from './map.ts';
+
+
+export function checkForLocation() {
+    if("geolocation" in navigator) {
+        console.log("Geolocation is available");
+    }
+    else {
+        console.log("Could not gather geolocation information");
+    }
+}
+
+
 
 export function setupMap() {
     let map = L.map('map').setView([36.09, -94.16], 10);
@@ -8,3 +21,7 @@ export function setupMap() {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 }
+
+
+// checkForLocation();
+setupMap();
