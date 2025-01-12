cd1c22ca1572251b454a1e1681d7bfbfb0b62920
marv <trujillomarvin@hotmail.com>
2025-01-11 21:12:21 -0600

    Feat:gather location

--- src/map.ts
+++ src/map.ts
@@ -1,27 +1,37 @@
 import L from "leaflet";
-// import { checkForLocation, setupMap } from './map.ts';
 
-
-export function checkForLocation() {
+export function checkLocationAvailability() {
     if("geolocation" in navigator) {
         console.log("Geolocation is available");
+        return true;
     }
     else {
         console.log("Could not gather geolocation information");
+        return false;
     }
 }
 
+export async function gatherLocation() {
+    function success(pos) {
+        setupMap(pos.coords);
+    }
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    return navigator.geolocation.getCurrentPosition(success, error);
+}
 
+export async function setupMap(pos) {
+    let lat = pos.latitude;
+    let long = pos.longitude;
+    console.log(lat, long)
 
-export function setupMap() {
-    let map = L.map('map').setView([36.09, -94.16], 10);
-    
+    let map = L.map('map').setView([lat, long], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 }
 
-
-// checkForLocation();
-setupMap();
+// let marker = L.marker([51.5, -0.09]).addTo(map);
+gatherLocation();
